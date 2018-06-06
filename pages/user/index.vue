a558b8c044d07347236c04c67c89c41dca87c0be
Sawyer <870756077@qq.com>
2018-06-06 10:57:57 +0800

    Initial commit

---
+++ pages/user/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="user">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        user-index
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+</style>
